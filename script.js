8028e24fe7b107bb551265ffba05aa180a1065cc
nicknijland2 <76453195+nicknijland2@users.noreply.github.com>
2021-01-03 22:53:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () { //Everything in this function runs when page is loaded completely
+	
+	//Smooth scrolling using jQuery
+	$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) { //Every a tag that links to a hash on the page
+		if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			//Target element
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if(target.length) { //If element exists
+				event.preventDefault(); //Do not go automatically to the element
+				$('html, body').animate({ //Go to the element smoothly
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					var $target = $(target);
+					$target.focus();
+					if($target.is(":focus")) {
+						return false;
+					} else {
+						$target.attr('tabindex','-1');
+						$target.focus();
+					};
+				});
+			}
+		}
+	});
+
+	document.addEventListener("scroll", function () {
+		document.querySelector("header > .-phone").style.cssText = (window.pageYOffset < 10 ? "opacity: 0; pointer-events: none;" : "");
+	});
+
+	document.getElementById('nsecrev').addEventListener("scroll", function () {
+		document.getElementById('nsecrev').classList.add("-nosel");
+	})
+});
